f5b399fb516790e3eef68ac8134130e77800bee4
guptan <nguptaa183@gmail.com>
2018-10-06 19:37:05 +0530

    changes

--- dbprac.py
+++ dbprac.py
@@ -1,13 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
+#---------FUNCTION TO GET HTML FROM URL---------#
+def getHTML(url):
+	response = requests.get(url)
+	return BeautifulSoup(response.content, 'html.parser')
+
+#---------GETTING IMDB LINK OF TV SHOW FROM GOOGLE---------#
 tv_show = input("Enter tv show: ")
-page = requests.get("https://www.google.co.in/search?q=" + tv_show)
-soup=BeautifulSoup(page.content,'html.parser')
+page = getHTML("https://www.google.co.in/search?q=" + tv_show)
 imdb_url=None
-for x in soup.findAll('cite'):
-    if 'https://www.imdb.com/title/' in x.text:
-        imdb_url=x.text
+for websites in page.findAll('cite'):
+    if 'https://www.imdb.com/title/' in websites.text:
+        imdb_url=websites.text
 print(imdb_url)
 
+#---------GETTING IMDB LINK OF TV SHOW FROM GOOGLE---------#
+# data['poster'] = html.find(attrs={'class': 'poster'}).find('img')['src']
 
