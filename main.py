95c112ad20b1760f58da74189094cde46653ffb3
guptan <nguptaa183@gmail.com>
2018-10-07 14:54:28 +0530

    changes

--- main.py
+++ main.py
@@ -1,44 +1,92 @@
-# Required Libraries
+#---------DEPENDENCIES---------#
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-import csv
-import re
+from datetime import datetime
 
-database = {}
-user_query = True
-while user_query:
-    email_query=True
-    while email_query:
-        email_address = input("Email address: ")
-        if '@' and '.' not in email_address:
-            print(" ------------------------------")
-            print("| Enter correct email address! |")
-            print(" ------------------------------")
-        else:
-            email_query=False
-    tv_series = list(map(str, input("TV Series: ").strip().split(',')))
-    database.update({email_address:tv_series})
-    query = input("More inputs(Y/N): ")
-    if user_query== 'n' or query == 'n':
-        user_query=False
+#---------USER INPUT---------#
+tv_show = input("Enter tv show: ").replace(' ', '_')
 
-===============PANDAS DATAFRAME==============#
-obj=pd.DataFrame(database)
-print(ovj)
+#---------CURRENT YEAR---------#
+now = datetime.now()
+current_year = now.year
 
-#==============CSV================#
-inputfile = csv.reader(open('database.csv', 'r'), delimiter='\t')
+#---------EXCEPTION HANDLING---------#
+try:
+    #---------FUNCTION TO GET HTML FROM URL---------#
+    def getHTML(url):
+        response = requests.get(url)
+        return BeautifulSoup(response.content, 'lxml')
 
-values=['gotham','suit']
+    #---------GETTING TITLE ID OF TV SHOW FROM IMDB SEARCH---------#
+    imdb_home_page = getHTML("https://www.imdb.com/find?" + tv_show)
+    find_show_id = imdb_home_page.findAll('td', class_='result_text')[0]
+    fetch_anchor_tag = find_show_id.find('a')
+    fetch_link = fetch_anchor_tag['href']
+    show_id = fetch_link[7:17]
 
-for row in inputfile:
-    # for key, value in database.items():
-    for tvshow in values:
-        if tvshow.title() in row:
-            print(row[0])
+    #---------GETTING TV SHOW RELEASE DATE IMDB---------#
+    imdb_page = getHTML("https://www.imdb.com/title/" + show_id)
+    show_date_year = imdb_page.find(
+        class_='seasons-and-year-nav').find_all('a')
+    show_years = []
+    for year in show_date_year:
+        year = year.text.strip()
+        if len(year) == 4:
+            show_years.append(year)
+    show_year = show_years[0]
+    show_last_year = show_years[1]
 
-# for key,value in database.items():
-#     for tvshow in value:
-#         print(tvshow)
+    #---------DISPLAYING STATUS OF SHOW---------#
+    if int(show_year) < current_year:
+        print("The show has finished streaming all its episodes.")
+    elif (int(show_year) > current_year) and (int(show_year)-1 != int(show_last_year)):
+        print("The next season begins in " + show_year + ".")
+    elif (int(show_year) > current_year) and (int(show_year)-1 == int(show_last_year)):
+        imdb_episode_url = "https://www.imdb.com/title/" + \
+            show_id + "episodes?year=" + show_last_year
+        imdb_episode_page = getHTML(imdb_episode_url)
+        airdate = []
+        show_date = imdb_episode_page.findAll(class_='airdate')
+        for ad in show_date:
+            ad = ad.text.strip()
+            if len(ad) != 4:
+                airdate.append(ad)
+        show_date = airdate[-1]
+        if '.' in show_date:
+            show_date = show_date.replace('.', '')
+        show_date = datetime.strptime(show_date, '%d %b %Y').date()
+        if show_date <= now.date():
+            print("The next season begins in " + show_year + ".")
+        elif show_date > now.date():
+            print('The next episode airs on ' + str(show_date) + '.')
 
+    elif int(show_year) == current_year:
+        imdb_episode_url = "https://www.imdb.com/title/" + \
+            show_id + "episodes?year=" + show_year
+        imdb_episode_page = getHTML(imdb_episode_url)
+        airdate = []
+        show_date = imdb_episode_page.findAll(class_='airdate')
+        for ad in show_date:
+            ad = ad.text.strip()
+            if len(ad) != 4:
+                airdate.append(ad)
+        show_date = airdate[-1]
+        if '.' in show_date:
+            show_date = show_date.replace('.', '')
+        show_date = datetime.strptime(show_date, '%d %b %Y').date()
+        if show_date <= now.date():
+            print('The show has finished streaming all its episodes of this year' +
+                  '(' + str(current_year) + ').')
+        elif show_date > now.date():
+            print('The next episode airs on ' + str(show_date) + '.')
+
+#---------EXCEPTION---------#
+except Exception as e:
+    print("\n        ERROR RETRIEVING DATA FROM IMDB\n")
+    print(" ------------------------------------------------")
+    print("|              POSSIBLE REASONS:                 |")
+    print("| --> NO INTERNET CONNECTION                     |")
+    print("| --> ENTERED TV SHOW DOESNOT EXIST              |")
+    print("| --> BAD RESPONSE FROM IMDB WHILE SCRAPING DATA |")
+    print("| --> ABSENCE OF DATA AT THE MOMENT              |")
+    print(" ------------------------------------------------")
