00d7de9755207a03be07dbce981b152a0eab2acc
guptan <nguptaa183@gmail.com>
2018-10-07 14:39:09 +0530

    changes

--- dbprac.py
+++ dbprac.py
@@ -11,21 +11,21 @@ tv_show = input("Enter tv show: ").replace(' ', '_')
 now = datetime.now()
 current_year = now.year
 
-#---------FUNCTION TO GET HTML FROM URL---------#
+#---------EXCEPTION HANDLING---------#
 try:
+    #---------FUNCTION TO GET HTML FROM URL---------#
     def getHTML(url):
         response = requests.get(url)
         return BeautifulSoup(response.content, 'lxml')
 
-
-#---------GETTING TITLE ID OF TV SHOW FROM IMDB SEARCH---------#
+    #---------GETTING TITLE ID OF TV SHOW FROM IMDB SEARCH---------#
     imdb_home_page = getHTML("https://www.imdb.com/find?" + tv_show)
     find_show_id = imdb_home_page.findAll('td', class_='result_text')[0]
     fetch_anchor_tag = find_show_id.find('a')
     fetch_link = fetch_anchor_tag['href']
     show_id = fetch_link[7:17]
 
-#---------GETTING TV SHOW RELEASE DATE IMDB---------#
+    #---------GETTING TV SHOW RELEASE DATE IMDB---------#
     imdb_page = getHTML("https://www.imdb.com/title/" + show_id)
     show_date_year = imdb_page.find(class_='seasons-and-year-nav').find_all('a')
     show_years=[]
@@ -36,8 +36,7 @@ try:
     show_year = show_years[0]
     show_last_year=show_years[1]
 
-#---------DISPLAYING STATUS OF SHOW---------#
-
+    #---------DISPLAYING STATUS OF SHOW---------#
     if int(show_year) < current_year:
         print("The show has finished streaming all its episodes.")
     elif (int(show_year) > current_year) and (int(show_year)-1 != int(show_last_year)):
@@ -78,12 +77,13 @@ try:
         elif show_date > now.date():
             print('The next episode airs on ' + str(show_date) + '.')
 
-
+#---------EXCEPTION---------#
 except Exception as e:
-    print("\n      ERROR RETRIEVING DATA FROM IMDB\n")
+    print("\n        ERROR RETRIEVING DATA FROM IMDB\n")
     print(" ------------------------------------------------")
     print("|              POSSIBLE REASONS:                 |")
     print("| --> NO INTERNET CONNECTION                     |")
+    print("| --> ENTERED TV SHOW DOESNOT EXIST              |")
     print("| --> BAD RESPONSE FROM IMDB WHILE SCRAPING DATA |")
     print("| --> ABSENCE OF DATA AT THE MOMENT              |")
     print(" ------------------------------------------------")
