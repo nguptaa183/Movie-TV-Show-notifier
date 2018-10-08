c8865c1230f0596e162d26b30307b0c7af0e5908
guptan <nguptaa183@gmail.com>
2018-10-09 01:15:44 +0530

    changes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from Modules import dates
 
-#---------USER INPUT---------#
-# movie_or_tv_show = input("Enter Movie or TV Show: ").replace(' ', '_')
-
+email_address = 'addicniku@gmail.com'
 movie_or_tv_show_list = ['lion', 'opopwers', 'game of throne', 'suits', 'Captain Marvel']
 for data in movie_or_tv_show_list:
     data = data.replace(' ', '_')
