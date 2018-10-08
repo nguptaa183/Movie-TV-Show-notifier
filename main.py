b5d85b84f9a98bae318575ac0f7e23b4f69e6e71
guptan <nguptaa183@gmail.com>
2018-10-09 01:11:35 +0530

    changes

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from Modules import dates
 
 #---------USER INPUT---------#
 # movie_or_tv_show = input("Enter Movie or TV Show: ").replace(' ', '_')
-ll = ['lion', 'opopwers', 'game of throne', 'suits', 'Captain Marvel']
-for z in ll:
-    z = z.replace(' ', '_')
-    dates.dates(z)
+
+movie_or_tv_show_list = ['lion', 'opopwers', 'game of throne', 'suits', 'Captain Marvel']
+for data in movie_or_tv_show_list:
+    data = data.replace(' ', '_')
+    dates.dates(data)
