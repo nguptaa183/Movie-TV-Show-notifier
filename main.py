31de41746401df33012143a822cc942363a45c11
guptan <nguptaa183@gmail.com>
2018-10-08 23:52:53 +0530

    changes

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def dates(movie_or_tv_show):
 
 #---------USER INPUT---------#
 # movie_or_tv_show = input("Enter Movie or TV Show: ").replace(' ', '_')
-ll = ['lion', 'game of throne', 'suits', 'Captain Marvel']
+ll = ['lion', 'opopwers','game of throne', 'suits', 'Captain Marvel']
 for z in ll:
     z=z.replace(' ','_')
     dates(z)
