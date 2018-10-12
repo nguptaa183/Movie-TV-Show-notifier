4348febd0ec67bb601061eebf235abe719a64ca3
guptan <nguptaa183@gmail.com>
2018-10-12 06:09:07 +0530

    changes

--- Database/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE movie_and_tvshows;
-
-CREATE TABLE Persons (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255) 
-);
